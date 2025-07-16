b0a602eb4da97206c951ced02051ee77cb69e33a
GrimmStone <grimmstonegaming@gmail.com>
2025-07-16 11:01:05 -0500

    Update script.js
    Fixes

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  const windowHeight = window.innerHeight;
+  reveals.forEach((el) => {
+    const elementTop = el.getBoundingClientRect().top;
+    if (elementTop < windowHeight * 0.85) {
+      el.classList.add("reveal-active");
+    }
+  });
+}
+
 window.addEventListener("DOMContentLoaded", revealOnScroll);
 window.addEventListener("load", revealOnScroll);
 window.addEventListener("scroll", revealOnScroll);
